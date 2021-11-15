fa2f6e471315e0d5084e4a1e79c740cda3daf5bf
pmRz00 <tpstiller@gmx.de>
2021-11-15 19:31:50 +0000

    order id only prime

--- src/python/app.py
+++ src/python/app.py
@@ -7,12 +7,8 @@ import os
 import requests
 import time
 
-dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
-dapr_url = "http://localhost:{}/v1.0/invoke/nodeapp/method/neworder".format(dapr_port)
 
-n = 0
-while True:
-    n += 1000
+def send_message(n):
     message = {"data": {"orderId": n}}
 
     try:
@@ -23,4 +19,19 @@ while True:
     except Exception as e:
         print(e, flush=True)
 
+dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
+dapr_url = "http://localhost:{}/v1.0/invoke/nodeapp/method/neworder".format(dapr_port)
+
+n = 0
+while True:
+    n += 1
+    # check if n is a prime number
+    if n % 2 == 0:
+        continue
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            break
+    else:
+        send_message(n)
     time.sleep(1)
+
