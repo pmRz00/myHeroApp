d5ab6b957c2c9142718a35dacdf273e404d07457
pmRz00 <tpstiller@gmx.de>
2021-11-15 16:52:39 +0000

    order id with fibonacci sequence number

--- src/python/app.py
+++ src/python/app.py
@@ -11,16 +11,12 @@ dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/invoke/nodeapp/method/neworder".format(dapr_port)
 
 n = 0
-while True:
-    n += 100
-    message = {"data": {"orderId": n}}
 
-    try:
-        response = requests.post(dapr_url, json=message, timeout=5)
-        if not response.ok:
-            print("HTTP %d => %s" % (response.status_code,
-                                     response.content.decode("utf-8")), flush=True)
-    except Exception as e:
-        print(e, flush=True)
-
+# for loop with fibonacci sequence
+for i in range(1, 1000):
+    n = n + i
+    print("Invoking neworder() with n = {}".format(n))
+    response = requests.post(dapr_url, json={"new order with id ": n})
+    print("Response: {}".format(response.text))
     time.sleep(1)
+
