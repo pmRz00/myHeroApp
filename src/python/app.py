2ca83dabdc9035ea68da00023399372cb1c75381
pmRz00 <tpstiller@gmx.de>
2021-11-15 16:59:30 +0000

    fixed json object and introduced prime sequence

--- src/python/app.py
+++ src/python/app.py
@@ -10,13 +10,17 @@ import time
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/invoke/nodeapp/method/neworder".format(dapr_port)
 
-n = 0
-
-# for loop with fibonacci sequence
-for i in range(1, 1000):
-    n = n + i
-    print("Invoking neworder() with n = {}".format(n))
-    response = requests.post(dapr_url, json={"new order with id ": n})
-    print("Response: {}".format(response.text))
-    time.sleep(1)
-
+# calculate next prime number in while loop
+n = 1
+while True:
+    n = n + 1
+    is_prime = True
+    for i in range(2, n):
+        if n % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print("Invoking neworder() with n = {}".format(n))
+        response = requests.post(dapr_url, json={"id": n})
+        print("Response: {}".format(response.text))
+        time.sleep(1)
