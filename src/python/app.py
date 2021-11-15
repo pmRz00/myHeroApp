0d260b7bbdfc7b0f623e7322e9ba4d756214ad7c
pmRz00 <tpstiller@gmx.de>
2021-11-15 17:16:48 +0000

    reset py app - incr by 1

--- src/python/app.py
+++ src/python/app.py
@@ -10,17 +10,17 @@ import time
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/invoke/nodeapp/method/neworder".format(dapr_port)
 
-# calculate next prime number in while loop
-n = 1
+n = 0
 while True:
-    n = n + 1
-    is_prime = True
-    for i in range(2, n):
-        if n % i == 0:
-            is_prime = False
-            break
-    if is_prime:
-        print("Invoking neworder() with n = {}".format(n))
-        response = requests.post(dapr_url, json={"data": {"orderId": n}})
-        print("Response: {}".format(response.text))
-        time.sleep(1)
+    n += 1
+    message = {"data": {"orderId": n}}
+
+    try:
+        response = requests.post(dapr_url, json=message, timeout=5)
+        if not response.ok:
+            print("HTTP %d => %s" % (response.status_code,
+                                     response.content.decode("utf-8")), flush=True)
+    except Exception as e:
+        print(e, flush=True)
+
+    time.sleep(1)
